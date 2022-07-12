e209876d4cb48c830063d4747b20564d673f6595
wdhen <100822887+wdhen@users.noreply.github.com>
2022-07-12 10:31:11 +1000

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,4 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.datafram(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
